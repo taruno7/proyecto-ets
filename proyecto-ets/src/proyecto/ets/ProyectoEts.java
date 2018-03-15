dec191a3249a05928c6e2dc5f2a39d34f137c051
alumno <alumno@DESKTOP-0U36439>
2018-03-15 23:57:17 +0100

    Este es el codigo principal creado por Tarun

--- proyecto-ets/src/proyecto/ets/ProyectoEts.java
+++ proyecto-ets/src/proyecto/ets/ProyectoEts.java
@@ -1,21 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyecto.ets;
 
-/**
- *
- * @author alumno
- */
-public class ProyectoEts {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+
+public class ProyectoEts {
+        static Scanner input=new Scanner(System.in);
+
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Escribe el primer numero: " );
+        double num1=input.nextDouble();
+
+        System.out.println("Escribe un operador: ");
+
+        //Espacio en blanco por el error nextLine() después de un nextDouble()
+        input.nextLine();
+        String op=input.nextLine();
+
+        System.out.println("Escribe el segundo numero: " );
+        double num2=input.nextDouble();
+        ProyectoEts.operacion(op, num1, num2);
+
     }
-    
+    public static void operacion(String op, double num1,double num2){
+        if ("sumar".equals(op)) {
+            double suma= num1+num2;
+            System.out.println("La suma es: " +suma);
+        }
+        if ("restar".equals(op)) {
+            double restar= num1-num2;
+            System.out.println("La suma es: " +restar);
+        }
+        if ("multiplicar".equals(op)) {
+            double multiplicar= num1*num2;
+            System.out.println("La suma es: " +multiplicar);
+        }
+        if ("diivir".equals(op)) {
+            double dividir= num1/num2;
+            System.out.println("La suma es: " +dividir);
+        }
+        else{
+            System.out.println("ERROR! NO HAS ESCRITO UNA OPERACIÓN VALIDA");
+        }
+    }
 }
